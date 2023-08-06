ea9645010a819525d84313ece138934c5eb48849
Thai <thaiphat_s@cmu.ac.th>
2023-08-06 22:48:45 +0700

    4.15

---
+++ src/components/CategoriesCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="category-card">
+      <p>{{ event?.category }}</p>
+      <p>{{ event?.organizer }}</p>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import type { EventItem } from '@/type'
+  import type { PropType } from 'vue'
+  const props = defineProps({
+    event: {
+      type: Object as PropType<EventItem>,
+      require: true
+    }
+  })
+  </script>
+  
+  <style scoped>
+  .category-card {
+    width: auto;
+    cursor: pointer;
+    margin-bottom: 18px;
+    text-align: end;
+  }
+  .category-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+  }
+  </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -10,5 +10,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+const x = "hello"
+console.log(x)
 
 app.mount('#app')
