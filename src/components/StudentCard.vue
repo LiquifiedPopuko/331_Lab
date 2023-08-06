eb51f06335cdd57f40957edb89b7b7a30a358863
Thai <thaiphat_s@cmu.ac.th>
2023-08-07 00:54:39 +0700

    Lab 2 Complete

---
+++ src/components/StudentCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="student-card">
+    <h3>{{ student?.name }} {{ student?.surname }}</h3>
+    <h4>GPA: {{ student?.gpa }}</h4>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { StudentItem } from '@/type'
+import type { PropType } from 'vue'
+const props = defineProps({
+  student: {
+    type: Object as PropType<StudentItem>,
+    require: true
+  }
+})
+</script>
+
+<style scoped>
+.student-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.student-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ src/views/StudentView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import StudentCard from '../components/StudentCard.vue'
+import StudentService from '@/services/StudentService'
+import { ref, type Ref } from 'vue'
+import type { StudentItem } from '@/type';
+const students: Ref<Array<StudentItem>> = ref([])
+StudentService.getEvent().then((response) => {
+  students.value = response.data
+})
+</script>
+
+<template>
+  <h1>Students</h1>
+  <main class="students">
+    <StudentCard v-for="student in students" :key="student.id" :student="student"></StudentCard>
+  </main>
+</template>
+
+<style scoped>
+.students {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+</style>
